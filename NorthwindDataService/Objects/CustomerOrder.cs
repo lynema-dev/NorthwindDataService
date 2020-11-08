86ddc58000312c82c5de801a715519f6de13201d
Matthew Lynes <63749896+lynema-dev@users.noreply.github.com>
2020-11-08 21:43:45 +0000

    Add files via upload

---
+++ NorthwindDataService/Objects/CustomerOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindDataService.Factories
+{
+    public class CustomerOrder 
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+    }
+
+    public class CustomerOrderList 
+    {
+        private readonly List<CustomerOrder> customerorderlist = new List<CustomerOrder>();
+
+        public IList<CustomerOrder> customerOrderList { get { return customerorderlist; } }
+        public string CustomerID { get; set; }
+
+    }
+}
---
+++ NorthwindDataService/Objects/CustomerOrderHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindDataService.Factories
+{
+    public class CustomerOrderHistory
+    {
+        public string ProductName { get; set; }
+        public int Total { get; set; }
+    }
+
+    public class CustomerOrderHistoryList
+    {
+        private readonly List<CustomerOrderHistory> customerorderhistorylist = new List<CustomerOrderHistory>();
+
+        public string CustomerID { get; set; }
+        public IList<CustomerOrderHistory> customerOrderHistoriesList { get { return customerorderhistorylist; } }
+
+    }
+}
