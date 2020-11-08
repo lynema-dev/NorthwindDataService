488bc1fe68b6188be9fc9483cf4e3aee7380847c
Matthew Lynes <63749896+lynema-dev@users.noreply.github.com>
2020-11-08 21:44:51 +0000

    Add files via upload

---
+++ NorthwindDataServiceTests/UnitTestsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthwindDataService.Factories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace NorthwindDataService.Tests
+{
+    [TestClass()]
+    public class UnitTestsTests
+    {
+        [TestMethod()]
+        public void CustomerOrderHistory()
+        {
+            var coh = new CustomerOrdersHistoryFactory(new NorthwindSqlDatabase());
+            var obj = coh.CreateDataObject("CONSH");
+
+            Assert.AreEqual(obj.CustomerID, "CONSH");
+            Assert.AreEqual(obj.customerOrderHistoriesList.Count, 7);
+        }
+
+        [TestMethod()]
+        public void NorthwindSqlDatabaseConnectionTest()
+        {
+            var northwindSqlDatabase = new NorthwindSqlDatabase();
+            Assert.IsNotNull(northwindSqlDatabase);
+        }
+
+    }
+}
